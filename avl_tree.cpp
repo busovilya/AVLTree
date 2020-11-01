c842ad4690f6e7c2c00f7cc06103151a04f10cb5
Ilya Busov <busov.ilyaa@gmail.com>
2020-11-01 15:36:36 +0200

    added deletion and fixed insertion

--- avl_tree.cpp
+++ avl_tree.cpp
@@ -25,7 +25,7 @@ Node* AVLTree::rotate_left(Node* node)
 	node->right = right_child->left;
 	right_child->left = node;
 	node->height = std::max(height(node->left), height(node->right));
-	right_child->height = std::max(height(right_child->left), height(right_child->right));
+	right_child->height = std::max(height(right_child->left), height(right_child->right)) + 1;
 	return right_child;
 }
 
@@ -34,7 +34,7 @@ Node* AVLTree::rotate_right(Node* node)
 	Node* left_child = node->left;
 	node->left = left_child->right;
 	left_child->right = node;
-	node->height = std::max(height(node->left), height(node->right));
+	node->height = std::max(height(node->left), height(node->right)) + 1;
 	return left_child;
 }
 
@@ -73,27 +73,81 @@ Node* AVLTree::insert_node(int value, Node* node)
 		return node;
 	
 	node->height = std::max(height(node->left), height(node->right)) + 1;
-	int balance = this->balance(node);
+    node = make_balance(node);	
+
+	return node;
+}
+
+void AVLTree::remove(int value) {
+	root = remove_node(value, root);
+}
+
+Node* AVLTree::remove_node(int value, Node* node) {
+	if(node == nullptr)
+		return nullptr;
+
+	if(value < node->key)	
+		node->left = remove_node(value, node->left);
+	else if(value > node->key)	
+		node->right = remove_node(value, node->right);
+	else
+	{
+		Node* tmp = node;
+		if(node->left != nullptr && node->right != nullptr)
+		{
+			Node* min_node = node->right;
+			while(min_node->left != nullptr)
+				min_node = min_node->left;
+			node->key = min_node->key;
+			node->right = remove_node(min_node->key, node->right);
+		}
+		else if(node->left != nullptr || node->right == nullptr)
+		{
+			node = node->left == nullptr ? node->right : node->left;
+			delete tmp;
+		}	
+	}
+
+    if(node == nullptr)
+        return nullptr;
+
+    node->height = std::max(height(node->left), height(node->right)) + 1;
+    node = make_balance(node);
+   
+	return node;	
+}
+
+Node* AVLTree::make_balance(Node* node)
+{
+    int balance = this->balance(node);
 	
-	if(balance > 1 && value < node->left->key)
+	if(balance > 1 && this->balance(node->left) > 0)
 	{
 		return rotate_right(node);
 	}
-	else if(balance > 1 && value > node->left->key)
+	else if(balance > 1 && this->balance(node->left) < 0)
 	{
 		node->left = rotate_left(node->left);
 		return rotate_right(node);
 	}
-	else if(balance < -1 && value > node->right->key)
+	else if(balance < -1 && this->balance(node->right) < 0)
 	{
 		return rotate_left(node);
 	}
-	else if(balance < -1 && value < node->right->key)
+	else if(balance < -1 && this->balance(node->right) > 0)
 	{
 		node->right = rotate_right(node->right);
 		return rotate_left(node);
 	}
 
-	return node;
+    return node;
 }
 
+int AVLTree::max()
+{
+    Node* node = root;
+
+    while(node->right != nullptr)
+        node = node->right;
+    return node->key;
+}
--- avl_tree.h
+++ avl_tree.h
@@ -2,16 +2,19 @@
 
 class AVLTree {
 private:
-	Node* root;
-	int height(Node* node);
-	int balance(Node* node);
-	Node* rotate_left(Node* node);	
-	Node* rotate_right(Node* node);	
-	Node* insert_node(int value, Node* root);
+    Node* root;
+    int height(Node* node);
+    int balance(Node* node);
+    Node* rotate_left(Node* node);	
+    Node* rotate_right(Node* node);	
+    Node* insert_node(int value, Node* root);
+    Node* remove_node(int value, Node* root);
+    Node* make_balance(Node* node, int value);
 public:
-	AVLTree();
-	void insert(int value);
-	void remove(int value);
-	bool search(int value);
-	Node* getRoot();
+    AVLTree();
+    void insert(int value);
+    void remove(int value);
+    bool search(int value);
+    Node* getRoot();
+    int max();
 };
